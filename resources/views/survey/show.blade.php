cd4f4d67ed778b254a4b7e704656808a5e102c11
Shams <shamsbouazra@gmail.com>
2020-03-30 00:24:09 +0100

    laravel2

--- resources/views/survey/show.blade.php
+++ resources/views/survey/show.blade.php
@@ -6,6 +6,7 @@
         <div class="col-md-8">
             <h1>{{$questionnaire->title}}</h1>
             <form action="/surveys/{{ $questionnaire->id }}-{{ Str::slug($questionnaire->title) }}" method="post">
+
             @csrf
             @foreach($questionnaire->questions as $key => $question)
                 <div class="card">
@@ -21,53 +22,52 @@
 
                         <ul class="list-group">
                             @foreach($question->answers as $answer)
-                               <label for="answer{{ $answer->id }}">
-                                   <li class="list-group-item">
-                                       <input type="radio" name="responses[{{$key}}][answer_id]" id="answer{{ $answer->id }}"
-                                              {{(old('responses.'.$key.'.answer_id')== $answer->id) ? 'checked' :''}}
-                                              class="mr-2" value="{{$answer->id}}">
-                                       {{$answer->answer}}
-                                       <input type="hidden" name="responses[{{$key}}][question_id ]" value="{{$question->id}}">
-                                   </li>
+                                <label for="answer{{ $answer->id }}">
+                                    <li class="list-group-item">
+                                        <input type="radio" name="responses[{{$key}}][answer_id]" id="answer{{ $answer->id }}"
+                                               {{(old('responses.'.$key.'.answer_id')== $answer->id) ? 'checked' :''}}
+                                               class="mr-2" value="{{$answer->id}}">
+                                        {{$answer->answer}}
+                                        <input type="hidden" name="responses[{{$key}}][question_id]" value="{{$question->id}}">
+                                    </li>
 
-                               </label>
+                                </label>
                             @endforeach
                         </ul>
                     </div>
 
                 </div>
-                @endforeach
-                <button class="btn btn-dark">Complete Survey</button>
-           <div class="card mt-4">
+            @endforeach
+            <div class="card mt-4">
                 <div class="card-header">Your Infromation</div>
 
                 <div class="card-body">
-                      <div class="form-group">
-                          <label for="name">Your Name</label>
-                          <input name="survey[name]" type="text" class="form-control" id="name" aria-describedby="nameHelp" placeholder="Enter Name">
-                          <small id="nameHelp" class="form-text text-muted">Hello what's your  name.</small>
-                          @error('name')
-                          <small class="text-danger">{{$message}}</small>
-                          @enderror
-                      </div>
-                      <div class="form-group">
-                          <label for="email">Your Email</label>
-                          <input name="survey[email]" type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter Email">
-                          <small id="emailHelp" class="form-text text-muted">Giving a purpose will increase responses.</small>
-                          @error('email')
-                          <small class="text-danger">{{$message}}</small>
-                          @enderror
+                <div class="form-group">
+                    <label for="name">Your Name</label>
+                    <input name="survey[name]" type="text" class="form-control" id="name" aria-describedby="nameHelp" placeholder="Enter Name">
+                    <small id="nameHelp" class="form-text text-muted">Hello what's your  name.</small>
+                    @error('name')
+                    <small class="text-danger">{{$message}}</small>
+                    @enderror
                 </div>
+                <div class="form-group">
+                    <label for="email">Your Email</label>
+                    <input name="survey[email]" type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter Email">
+                    <small id="emailHelp" class="form-text text-muted">Giving a purpose will increase responses.</small>
+                    @error('email')
+                    <small class="text-danger">{{$message}}</small>
+                    @enderror
+                </div>
+                </div>
+                <div>
+                    <button class="btn btn-dark">Complete Survey</button>
 
-                      <div>
-                          <button class="btn btn-dark">Complete Survey</button>
 
-                      </div>
-            </div>
+                </div>
             </div>
             </form>
-
         </div>
+            </div>
     </div>
-</div>
+
 @endsection
